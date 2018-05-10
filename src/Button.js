f11143e1e403eb89be9210fb86d6dd30fe938a71
da-matsumoto <da.mats1990@gmail.com>
2018-05-10 22:33:26 +0900

    buttonを作成

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+    border: 2px;
+    background-color: #102449;
+    color: #FFFFFF;
+    font-size: 16px;
+    margin-top: 20px;
+`
+
+const Add = () => {
+    return(
+        <Button>追加</Button>
+    )
+}
+
+export default Add
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Add from './Button'
+
+ReactDOM.render(<Add />, document.getElementById('root'));
